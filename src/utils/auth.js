be877dc8a77e7848527bd8c84aa6c9bd0a35080a
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-11 11:22:14 +0400

    add credentials

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,6 +10,7 @@ function checkResponse(res) {
 export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`, {
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json'
         },
@@ -21,6 +22,7 @@ export const register = (email, password) => {
 export const authorize = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json'
         },
@@ -31,6 +33,7 @@ export const authorize = (email, password) => {
 export const checkToken = () => {
     return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',

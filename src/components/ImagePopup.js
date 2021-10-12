1e0e9fde78ed3fa97198a61189f236262aed1489
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-12 13:21:31 +0400

    Update ImagePopup.js

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,6 +1,6 @@
 function ImagePopup(props) {
     return (
-        <div className={`popup popup-${props.name} ${props.isOpen ? 'popup_opened' : ''}`} onClick={props.handleOverlay}>
+        <div className={`popup popup-${props.name} ${props.card.link ? 'popup_opened' : ''}`} onClick={props.handleOverlay}>
             <div className='popup__image'>
                 <img className='popup__photo' src={props.card.link} alt={props.card.name} />
                 <h2 className='popup__caption'>{props.card.name}</h2>

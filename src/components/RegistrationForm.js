736c4a0f3aee595d0b4c7e22e2f3dcd514ae4273
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-12 12:15:57 +0400

    Update RegistrationForm.js

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,25 +3,17 @@ import { Link } from 'react-router-dom';
 
 function RegistrationForm(props) {
     return (
-        <section className='registration'>
-            <h2 className='registration__title'>{props.title}</h2>
-            <form
-                name={props.name}
-                onSubmit={props.onSubmit}
-                noValidate>
-                <fieldset className='registration__form'>
+        <section className="registration">
+            <h2 className="registration__title">{props.title}</h2>
+            <form name={props.name} onSubmit={props.onSubmit} noValidate>
+                <fieldset className="registration__form">
                     {props.children}
-                    <button
-                        className='registration__submit-button'
-                        type='submit'
-                        name='submit'>{props.buttonText}</button>
+                    <button className="registration__submit-button" type="submit" name='submit'>{props.buttonText}</button>
 
                     {props.name === 'register' &&
-                        <div className='registration__login'>
-                            <p className='registration__login-caption'>Уже зарегистрированы?&nbsp;</p>
-                            <Link
-                                to='/signin'
-                                className='registration__login-link'>Войти</Link>
+                        <div className="registration__login">
+                            <p className="registration__login-caption">Уже зарегистрированы?&nbsp;</p>
+                            <Link to="/signin" className="registration__login-link">Войти</Link>
                         </div>
                     }
                 </fieldset>
